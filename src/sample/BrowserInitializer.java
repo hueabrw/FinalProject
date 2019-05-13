a97402044a582f8f19656ae8cb1e26e33fe2a05d
Abraham Huerta <Abrhuerta@gmail.com>
2019-05-12 21:27:36 -0500

    SOoooo messy

--- src/sample/BrowserInitializer.java
+++ src/sample/BrowserInitializer.java
@@ -1,11 +1,60 @@
 package sample;
 
 import com.teamdev.jxbrowser.chromium.JSObject;
+import com.teamdev.jxbrowser.chromium.JSValue;
+import javafx.application.Platform;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.jsoup.Jsoup;
+
+import javax.swing.text.Document;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class BrowserInitializer {
 
-    public void onPolyClick(JSObject coord){
-        System.out.println(coord);
+    DeveloperPageController dev;
+    JSONArray jsonFile;
+    JSONArray array;
+
+    public BrowserInitializer(DeveloperPageController _dev, String jsonString){
+        dev = _dev;
+        try {
+            Object obj = new JSONParser().parse(jsonString);
+            array = (JSONArray)obj;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void onPolyClick(double id){
+        dev.maximize();
+        System.out.println(id);
+        dev.populatePanel(grabParcel(((int) id)));
+    }
+
+
+
+    public Parcel grabParcel(int id){
+        for(int i = 0; i < array.size(); i++){
+            if(((JSONObject)array.get(i)).get("ID").toString().equals(String.valueOf(id))){
+                System.out.println("made it");
+                JSONObject parObj = (JSONObject)array.get(i);
+                JSONArray coordArray = ((JSONArray)parObj.get("Coord"));
+                Coord[] points = new Coord[coordArray.size()];
+                for(int j = 0; j < coordArray.size() ; j++){
+                    Coord point = new Coord(Double.valueOf((((JSONObject)coordArray.get(j)).get("lat")).toString()), Double.valueOf((((JSONObject)coordArray.get(j)).get("lng")).toString()));
+                    points[j] = point;
+                }
+                Parcel parcel = new Parcel((Integer.valueOf(parObj.get("ID").toString())),points);
+                return parcel;
+            }
+        }
+        return null;
     }
 
 }
--- src/sample/DeveloperPageController.java
+++ src/sample/DeveloperPageController.java
@@ -6,7 +6,13 @@ import java.util.ResourceBundle;
 
 import com.teamdev.jxbrowser.chromium.*;
 import com.teamdev.jxbrowser.chromium.events.*;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.*;
 import org.jsoup.Jsoup;
@@ -23,13 +29,36 @@ import javafx.stage.Stage;
 import com.teamdev.jxbrowser.chromium.internal.Environment;
 import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 
 public class DeveloperPageController implements Initializable{
 
+    public BrowserInitializer bi;
+    public ImageView fileImage1;
+    public Label fileLabel1;
+    FileChooser fileChooser;
+    Stage stage;
+    Connection dbcon;
 
     @FXML
-    private TextField textField;
+    public Button minimize;
+    @FXML
+    public Pane parcelInfo;
+    @FXML
+    public VBox vbox;
+    @FXML
+    public Label number;
+    @FXML
+    public Label id;
 
     @FXML
     private BrowserView browserView;
@@ -37,6 +66,13 @@ public class DeveloperPageController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle rb){
+        try {
+            dbcon = DriverManager.getConnection("https://www.db4free.net/phpMyAdmin/db_structure.php?server=1&db=parceldb", "abrhuerta", "Green123");
+            System.out.println("Connected to db");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        fileChooser = new FileChooser();
         browser = browserView.getBrowser();
         browser.loadURL("file:C:/code/Java/FinalProject/src/sample/googlemaps.html");
         run();
@@ -50,8 +86,9 @@ public class DeveloperPageController implements Initializable{
                 System.out.println("Message: " + event.getMessage());
             }
         });
+        FileInputStream is = null;
         try {
-            InputStream is = new FileInputStream("C:/code/Java/FinalProject/src/sample/test.json");
+            is = new FileInputStream("C:/code/Java/FinalProject/src/sample/test.json");
 
             BufferedReader buf = new BufferedReader(new InputStreamReader(is));
             String line = buf.readLine(); StringBuilder sb = new StringBuilder();
@@ -64,21 +101,99 @@ public class DeveloperPageController implements Initializable{
             String fileAsString = sb.toString();
 
 
+            bi = new BrowserInitializer(this, fileAsString);
+
+
             browser.addScriptContextListener(new ScriptContextAdapter() {
                 @Override
                 public void onScriptContextCreated(ScriptContextEvent event) {
                     JSValue window = browser.executeJavaScriptAndReturnValue("window");
                     window.asObject().setProperty("jsonData", fileAsString);
-                    window.asObject().setProperty("PolyInteract", new BrowserInitializer());
-
+                    window.asObject().setProperty("PolyInteract", bi);
                 }
 
             });
 
+            is.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public void maximize(){
+        parcelInfo.setVisible(true);
+        parcelInfo.setPrefWidth(200);
+    }
 
+    public void upload(ActionEvent actionEvent) {
+        stage = (Stage) parcelInfo.getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            openFile(file);
+        }
+        try {
+            if (!fileLabel1.getText().equals("No Files")) {
+                ImageView imageView = new ImageView();
+                imageView.setFitHeight(50);
+                imageView.setFitWidth(50);
+                Label label = new Label();
+                label.setPadding(new Insets(20,0,0,10));
+                HBox hbox = new HBox();
+                hbox.getChildren().add(0,imageView);
+                hbox.getChildren().add(1,label);
+                label.setText(file.getName());
+                Image image = new Image(file.toURI().toString());
+                imageView.setImage(image);
+                vbox.getChildren().add(vbox.getChildren().size(), hbox);
+
+            } else {
+                fileLabel1.setText(file.getName());
+                Image image = new Image(file.toURI().toString());
+                fileImage1.setImage(image);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void preview(ActionEvent actionEvent) {
+    }
+
+    public void minimize(ActionEvent actionEvent) {
+        parcelInfo.setVisible(false);
+        parcelInfo.setPrefWidth(0);
+    }
+
+    public void logout(ActionEvent actionEvent) {
+        try{
+            stage = (Stage) parcelInfo.getScene().getWindow();
+            Pane pane = FXMLLoader.load(
+                    LoginController.class.getResource("login.fxml"));
+
+            stage.setTitle("Login");
+            stage.setScene(new Scene(pane, 630, 580));
+            stage.show();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void populatePanel(Parcel parcel) {
+        Platform.runLater(new Runnable() {
+            @Override public void run() {
+                id.setText(String.valueOf(parcel.id));
+            }
+        });
+    }
+
+    private void openFile(File file){
+        try{
+            System.out.println(file.getCanonicalPath());
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
